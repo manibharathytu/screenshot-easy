6fb24440faab318f8daf0a0e9a14d728915b9e75
mani <manibharathytu5194@gmail.com>
2020-05-19 15:47:48 +0530

    fully working app

--- main.js
+++ main.js
@@ -1,7 +1,49 @@
 const clipboardy = require('clipboardy');
 
-// Copy
-clipboardy.writeSync('🦄');
 
-// Paste
-clipboardy.readSync();
+var watch = require('node-watch');
+
+
+var awsCli = require('aws-cli-js');
+var Options = awsCli.Options;
+var Aws = awsCli.Aws;
+
+var options = new Options(
+    /* accessKey    */ 
+    /* secretKey    */ 
+);
+
+var aws = new Aws(options);
+
+let lastTime = 0;
+const oneSecondRuleCheck = function () { // The goal of this wait time is whenever a file is updaed its triggering 2 or 3 updates back to back. To prevent
+  const curTime = Date.now()
+  if (curTime - lastTime > 1000) { lastTime = curTime; return false; }
+  else return true;
+}
+
+
+const onChangeDetection = function (evt, name) {
+
+  if (evt == 'update' && !name.startsWith('Screenshot')) {
+    console.log('%s %s', name, evt);
+    if(oneSecondRuleCheck()) return;
+    const fileName = name.split('/').pop()
+    const upload_cmd = "s3 cp \"" + name + "\" s3://screenshots-auto --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers"
+    clipboardy.writeSync('https://screenshots-auto.s3.us-west-2.amazonaws.com/' + fileName); // copy to clipboard
+
+    aws.command(upload_cmd).then(function (data) {
+      console.log('data = ', data);
+    });
+
+  }
+}
+
+watch('/home/mani/Downloads', { recursive: false }, onChangeDetection);
+watch('/home/mani/Pictures', { recursive: false }, onChangeDetection);
+
+
+  // ////
+  // silly :
+
+  // typeof(name)=='string' && 
---
+++ start.sh
@@ -0,0 +1,4 @@
+THIS_DIR=`dirname "$0"` #second answer in this link. https://stackoverflow.com/questions/59895/how-to-get-the-source-directory-of-a-bash-script-from-within-the-script-itself
+cd $THIS_DIR
+
+node main.js &
